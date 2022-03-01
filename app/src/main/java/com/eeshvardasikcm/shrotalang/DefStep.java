c9326a96acf12c324a9a6cfc383fabb92223df4b
Erik Ward (Eeshwar Das) <ishwariandeeshwar49362108963@gmail.com>
2022-02-28 18:17:30 -0800

    Added new language constants in new StepsNavigation.shrota class
    SHROTA_STAGE_CHECK_KEY could possibly be used in Ayurveda to check the depth< of the early stages the process of disease development.

--- app/src/main/java/com/eeshvardasikcm/shrotalang/DefStep.java
+++ app/src/main/java/com/eeshvardasikcm/shrotalang/DefStep.java
@@ -1,7 +1,7 @@
 package com.eeshvardasikcm.shrotalang;
 
 /**
- * defstep is used to define steps inside the steps.shrota class
+ * defstep keyword is used to define steps inside the steps.shrota class
  * and helps alleviate the disabled subclassing in shrota-lang.
  * SubStepping is the ShrotaModel in String defined flowchart.shrota steps.shrota
  */
